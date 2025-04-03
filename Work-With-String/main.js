d79e03209b920183ac91cbc63a3ee3d9aa8395a6
Nguyen Hong Son <166919875+NHSon05@users.noreply.github.com>
2025-04-03 17:21:37 +0700

    Add len lai

---
+++ Work-With-String/main.js
@@ -0,0 +1,11 @@
+// Declaration function
+function showMessage(){
+    console.log('Declaration function');
+}
+showMessage("hehehe");
+
+// Expression function
+var showMessage2 = function() {
+    console.log('Expression function');
+}
+showMessage2('hihi');
